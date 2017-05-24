28fd8adfa8df02ce20839d940514ee8ddb4d6db2
meng <wulalameng@gmail.com>
2017-05-23 20:28:42 -0400

    make btn larger

--- RWDF_L2_Start/js/app.js
+++ RWDF_L2_Start/js/app.js
@@ -1,5 +1,11 @@
-var menu = document.querySelector("#menu");
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
 var nav = document.querySelector('.nav');
-menu.addEventListener('click', function(){
+menu.addEventListener('click', function(e){
   nav.classList.toggle('open');
+  e.stopPropagation();
 });
+
+main.addEventListener('click', function(){
+  nav.classList.remove('open');
+})
